057990b9203b042c50b57d969ee865bdb6cc72cf
sgt39007 <sgt39007@163.com>
2016-06-08 09:47:35 +0800

    1

--- MyFirstApp/app/src/main/java/com/example/echo/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/example/echo/myfirstapp/MainActivity.java
@@ -1,13 +1,63 @@
 package com.example.echo.myfirstapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("MainActivity", "Task id is" + getTaskId());
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button)findViewById(R.id.button_1);
+        if (button1 != null) {
+            button1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+    //                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+    //                Intent intent = new Intent("com.example.echo.ACTION_START");
+    //                intent.addCategory("com.example.echo.MY_CATEGORY");
+
+    //                Intent intent = new Intent(Intent.ACTION_VIEW);
+    //                intent.setData(Uri.parse("http://www.baidu.com"));
+
+    //                Intent intent = new Intent(Intent.ACTION_DIAL);
+    //                intent.setData(Uri.parse("tel:10086"));
+
+    //                String data = "Hello Second";
+    //                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+    //                intent.putExtra("extra_data", data);
+
+//                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+//                    startActivityForResult(intent, 1);
+
+                    Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("First", returnedData);
+                }
+                break;
+            default:
+        }
     }
 }
