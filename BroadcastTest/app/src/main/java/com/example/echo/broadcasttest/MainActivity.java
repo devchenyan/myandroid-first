ad1d9f6e7f4da85c42a544b736681e6c7e1530b9
sgt39007 <sgt39007@163.com>
2016-06-13 16:59:02 +0800

    +BroadcastBestPractice

--- BroadcastTest/app/src/main/java/com/example/echo/broadcasttest/MainActivity.java
+++ BroadcastTest/app/src/main/java/com/example/echo/broadcasttest/MainActivity.java
@@ -1,28 +1,59 @@
 package com.example.echo.broadcasttest;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private IntentFilter intentFilter;
+
+    private LocalBroadcastManager localBroadcastManager;
+
+    private LocalReceiver localReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
+
         Button button = (Button) findViewById(R.id.button);
         if (button != null) {
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent intent = new Intent("com.example.echo.broadacsttest.MY_BROADCAST");
-                    sendBroadcast(intent);
+                    Intent intent = new Intent("com.example.echo.broadacsttest.LOCAL_BROADCAST");
+                    localBroadcastManager.sendBroadcast(intent);
                 }
             });
         }
 
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("com.example.echo.broadacsttest.LOCAL_BROADCAST");
+
+        localReceiver  = new LocalReceiver();
+        localBroadcastManager.registerReceiver(localReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        localBroadcastManager.unregisterReceiver(localReceiver);
+    }
+
+    class LocalReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "receive local", Toast.LENGTH_SHORT).show();
+        }
     }
 }
