e618f519537ccba0b84724cc86ed313c20b600ef
sgt39007 <sgt39007@163.com>
2016-06-13 16:10:49 +0800

    my broadcast

--- BroadcastTest/app/src/main/java/com/example/echo/broadcasttest/MainActivity.java
+++ BroadcastTest/app/src/main/java/com/example/echo/broadcasttest/MainActivity.java
@@ -1,49 +1,28 @@
 package com.example.echo.broadcasttest;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    private NetworkChangeReceiver networkChangeReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
-
-        networkChangeReceiver = new NetworkChangeReceiver();
-
-        registerReceiver(networkChangeReceiver, intentFilter);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(networkChangeReceiver);
-    }
-
-    class NetworkChangeReceiver extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-
-            if (networkInfo != null && networkInfo.isAvailable()) {
-                Toast.makeText(context, "network is available", Toast.LENGTH_SHORT).show();
-            }else {
-                Toast.makeText(context, "network is unavailable", Toast.LENGTH_SHORT).show();
-            }
+        Button button = (Button) findViewById(R.id.button);
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent("com.example.echo.broadacsttest.MY_BROADCAST");
+                    sendBroadcast(intent);
+                }
+            });
         }
+
     }
 }
