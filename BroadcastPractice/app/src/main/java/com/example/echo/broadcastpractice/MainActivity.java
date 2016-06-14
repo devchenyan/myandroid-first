f0aa76896be6f30ac7e1118c47bc8099f6ec480c
sgt39007 <sgt39007@163.com>
2016-06-14 13:40:29 +0800

    end

--- BroadcastPractice/app/src/main/java/com/example/echo/broadcastpractice/MainActivity.java
+++ BroadcastPractice/app/src/main/java/com/example/echo/broadcastpractice/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.echo.broadcastpractice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button forceOffline = (Button) findViewById(R.id.force_offline);
+        if (forceOffline != null) {
+            forceOffline.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent("com.example.broadcastpractice.FORCE_OFFLINE");
+                    sendBroadcast(intent);
+                }
+            });
+        }
     }
 }
