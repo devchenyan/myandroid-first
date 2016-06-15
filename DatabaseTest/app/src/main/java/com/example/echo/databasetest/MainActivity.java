da476a1d02f2c2d7a3ea5d4cd5d94477623461e9
sgt39007 <sgt39007@163.com>
2016-06-15 15:13:17 +0800

    insert

--- DatabaseTest/app/src/main/java/com/example/echo/databasetest/MainActivity.java
+++ DatabaseTest/app/src/main/java/com/example/echo/databasetest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.echo.databasetest;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,6 +24,33 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     dbHelper.getWritableDatabase();
+                }
+            });
+        }
+
+        Button addData = (Button) findViewById(R.id.add_data);
+        if (addData != null) {
+            addData.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    SQLiteDatabase db = dbHelper.getWritableDatabase();
+                    ContentValues values = new ContentValues();
+//                    装第一组数据
+                    values.put("name", "The First");
+                    values.put("author", "Kiyola");
+                    values.put("pages", 455);
+                    values.put("price", 16.96);
+//                    插入
+                    db.insert("Book", null, values);
+                    values.clear();
+
+//                    装第二组数据
+                    values.put("name", "The Second");
+                    values.put("author", "Michael");
+                    values.put("pages", 355);
+                    values.put("price", 15.46);
+
+                    db.insert("Book", null, values);
                 }
             });
         }
--- DatabaseTest/app/src/main/java/com/example/echo/databasetest/MyDatabaseHelper.java
+++ DatabaseTest/app/src/main/java/com/example/echo/databasetest/MyDatabaseHelper.java
@@ -8,11 +8,16 @@ import android.widget.Toast;
 public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     public static final String CREATE_BOOK = "create table book ("
-            + "id integer primary key autoincrement"
-            + "author text"
-            + "price real"
-            + "pages integer"
+            + "id integer primary key autoincrement,"
+            + "author text,"
+            + "price real,"
+            + "pages integer,"
             + "name text)";
+
+    public static final String CREATE_CATEGORY = "create table book ("
+            + "id integer primary key autoincrement,"
+            + "category_name text,"
+            + "category_code integer)";
 
     private Context mContext;
 
@@ -24,11 +29,14 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_BOOK);
+        db.execSQL(CREATE_CATEGORY);
         Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("drop table if exists Book");
+        db.execSQL("drop table if exists Category");
+        onCreate(db);
     }
 }
