9be944953a149a5f1fbe70e3383e4d3bba954d12
sgt39007 <sgt39007@163.com>
2016-06-16 12:10:01 +0800

    threadtest

---
+++ DatabaseTest/app/src/main/java/com/example/echo/databasetest/DatabaseProvider.java
@@ -0,0 +1,78 @@
+package com.example.echo.databasetest;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+public class DatabaseProvider extends ContentProvider {
+
+    public static final int BOOK_DIR = 0;
+
+    public static final int BOOK_ITEM = 1;
+
+    public static final int CATEGORY_DIR = 2;
+
+    public static final int CATEGORY_ITEM = 3;
+
+    public static final String AUTHORITY = "com.example.echo.databasetest.provider";
+
+    private static UriMatcher uriMatcher;
+
+    private MyDatabaseHelper dbHelper;
+
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(AUTHORITY, "book", BOOK_DIR);
+        uriMatcher.addURI(AUTHORITY, "book/#", BOOK_ITEM);
+        uriMatcher.addURI(AUTHORITY, "category", CATEGORY_DIR);
+        uriMatcher.addURI(AUTHORITY, "category/#", CATEGORY_ITEM);
+    }
+    @Override
+    public boolean onCreate() {
+        dbHelper = new MyDatabaseHelper(getContext(), "BookStroe.db", null, 2);
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = null;
+
+        switch (uriMatcher.match(uri)) {
+            case BOOK_DIR:
+                cursor = db.query("Book", projection, selection, selectionArgs, null, null, sortOrder);
+                break;
+            case BOOK_ITEM:
+                String bookId = uri.getPathSegments().get(1);
+                cursor = db.query("Book", projection, selection, selectionArgs, null, null, sortOrder);
+        }
+        return null;
+    }
+}
